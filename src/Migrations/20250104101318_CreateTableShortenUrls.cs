821671c6fcaaed5b57a1bbdb875fe6bd9233f5c6
AzureAD\DewaGdeMahendraPutra <d.putra@lakeba.com>
2025-01-04 18:17:03 +0800

    add new migration

---
+++ src/Migrations/20250104101318_CreateTableShortenUrls.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableShortenUrls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShortenUrls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LongUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShortUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShortenUrls", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortenUrls_Code",
+                table: "ShortenUrls",
+                column: "Code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShortenUrls");
+        }
+    }
+}
